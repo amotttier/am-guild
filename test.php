abaf0352cdeab1de6c36c46c5dcf478d77597736
amotttier <alex_mottier@hotmail.com>
2015-08-18 22:18:18 +0200

    2eme test

--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php
-
   echo "ceci est mon premier test GitHub";
-  echo "<div style="font-size:12px;font-family:Arial;">ceci est mon premier test GitHub</div>";
+  echo '<div style="font-size:12px;font-family:Arial;">ceci est mon deuxième test GitHub</div>';
 ?>
